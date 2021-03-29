e5ff8ea612a29fd184a001467663ce84f24dd6b1
fenghao qi <fqii0002@student.monash.edu>
2021-03-30 02:28:08 +0800

    Add AQI cell view class

---
+++ FIT5120_MyAllerGone/Home Screen/View/AQICollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AQICollectionViewCell.swift
+//  FIT5120_MyAllerGone
+//
+//  Created by Qfh on 29/3/21.
+//
+
+import UIKit
+
+class AQICollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var AQIView: UIView!
+    
+    
+}
